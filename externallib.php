149bb91856a2d547ec4a98dca9a4349f9b0a5ce2
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-10-04 12:07:22 +0600

    get_course_image function implemented

--- externallib.php
+++ externallib.php
@@ -22,3 +22,25 @@
  * @copyright 2022, Md. Shofiul Islam
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+require_once(__DIR__ . '/../../../config.php');
+require_once($CFG->libdir . '/filelib.php');
+
+
+
+function get_course_image($id)
+{
+    $url = '';
+
+    $context = context_course::instance($id);
+    $fs = get_file_storage();
+    $files = $fs->get_area_files($context->id, 'course', 'overviewfiles', 0);
+
+    foreach ($files as $f) {
+        if ($f->is_valid_image()) {
+            $url = moodle_url::make_pluginfile_url($f->get_contextid(), $f->get_component(), $f->get_filearea(), null, $f->get_filepath(), $f->get_filename(), false);
+        }
+    }
+
+    return $url;
+}
