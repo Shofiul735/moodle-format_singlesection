ce0322e1b6c1a0b12398ba6551a5d73bfecf356a
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-10-04 11:58:29 +0600

    Basic course format created

---
+++ lang/en/format_newsection.php
@@ -0,0 +1,41 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_newsection', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   format_newsection
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addsections'] = 'Add newsection';
+$string['currentsection'] = 'This newsection';
+$string['editsection'] = 'Edit newsection';
+$string['editsectionname'] = 'Edit newsection name';
+$string['deletesection'] = 'Delete newsection';
+$string['newsectionname'] = 'New name for newsection {$a}';
+$string['sectionname'] = 'newsection';
+$string['pluginname'] = 'newsection format';
+$string['section0name'] = 'General';
+$string['page-course-view-newsection'] = 'Any course main page in newsection format';
+$string['page-course-view-newsection-x'] = 'Any course page in newsection format';
+$string['hidefromothers'] = 'Hide newsection';
+$string['showfromothers'] = 'Show newsection';
+$string['automaticenddate'] = 'Calculate the end date from the number of sections';
+$string['automaticenddate_help'] = 'If enabled, the end date for the course will be automatically calculated from the number of sections and the course start date.';
+$string['privacy:metadata'] = 'The newsection format plugin does not store any personal data.';
