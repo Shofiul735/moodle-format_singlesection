b124991bba77705a0171e2c7f35a2db5dcc209db
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-10-04 12:27:51 +0600

    display function implemented

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -65,4 +65,78 @@ class renderer extends section_renderer
     {
         return $this->render(course_get_format($course)->inplace_editable_render_section_name($section, false));
     }
+
+
+
+    public function display($course, $orphaned)
+    {
+        global $USER, $COURSE, $PAGE;
+        $format = course_get_format($course);
+        $modinfo = $format->get_modinfo();
+        $courseinfo = $format->get_course();
+        $cmlistclass = $format->get_output_classname('content\\section\\cmlist');
+
+        // Checking if the user is a student
+        $isstudent = user_has_role_assignment($USER->id, 5);
+
+
+
+        $output = '';
+        if ($isstudent) {
+
+            $url = get_first_activity_url($modinfo->get_cms());
+            $url = resumed_course_activity_url($course, $USER->id, $modinfo->get_cms()) ?? $url;
+
+            $imageurl = get_course_image($courseinfo->id);
+            $progress = get_progress($courseinfo);
+
+            $buttontext = null;
+            if ($progress === 0) {
+                $buttontext = 'Start Course';
+            } else {
+                $buttontext = 'Resume Course';
+            }
+
+            echo html_writer::start_div();
+            echo html_writer::tag('h1', $courseinfo->fullname);
+            echo html_writer::end_div();
+            echo html_writer::img($imageurl, 'course image', ['width' => '100%']);
+            echo html_writer::start_div('text-justify mt-5');
+            echo html_writer::tag('h3', 'Course Summary:', ['class' => 'text-primary']);
+            echo html_writer::tag('h6', $courseinfo->summary, ['class' => 'text-secondary']);
+            echo html_writer::start_div('d-flex justify-content-between align-items-center');
+            echo html_writer::tag('a', $buttontext, ['href' => $url, 'class' => 'btn btn-outline-success', 'type' => 'submit']);
+            echo html_writer::start_div();
+            echo html_writer::tag('p', "Course Progress: {$progress}% ", ['class' => 'text-success']);
+            echo html_writer::tag('progress', "{$progress}%", ['max' => 100, 'value' => $progress]);
+            echo html_writer::end_div();
+            echo html_writer::end_div();
+            echo html_writer::end_div();
+
+            return;
+        } else {
+            if ($orphaned) {
+                if (!empty($modinfo->sections[1])) {
+                    $output .= $this->output->heading(get_string('orphaned', 'format_newsection'), 3, 'sectionname');
+                    $output .= $this->output->box(get_string('orphanedwarning', 'format_newsection'));
+
+                    $section = $modinfo->get_section_info(1);
+                    $output .= $this->render(new $cmlistclass($format, $section));
+                }
+            } else {
+                $section = $modinfo->get_section_info(0);
+                $output .= $this->render(new $cmlistclass($format, $section));
+
+                if (empty($modinfo->sections[0]) && course_get_format($course)) {
+                    // Course format was unable to automatically redirect to add module page.
+                    $output .= $this->course_section_add_cm_control($course, 0, 0);
+                }
+            }
+            return $output;
+        }
+
+
+
+        return $output;
+    }
 }
